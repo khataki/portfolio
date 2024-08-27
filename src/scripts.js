55565cb74b2a34668c1e5fe0f5a0558af6bf7ef6
khataki <84592431+khataki@users.noreply.github.com>
2024-08-27 16:18:37 +0300

    1.0

--- src/scripts.js
+++ src/scripts.js
@@ -60,10 +60,63 @@ function setLanguage(lang) {
             document.getElementById('design_title').textContent = data.design_title;
             document.getElementById('figma_adobe').textContent = data.design_skills.figma_adobe;
             document.getElementById('blender').textContent = data.design_skills.blender;
-            // Добавьте сюда обновление других элементов, если необходимо
+            // Для проекта "Оптовый магазин одежды МАХИС"
+            document.querySelector('h3.project-maxis-title').textContent = data["project_title_maxis"];
+            document.querySelector('.description.project-maxis-role p:first-child').innerHTML = data["role"];
+            const maxisDescriptionList = document.querySelector('.description.project-maxis ul');
+            maxisDescriptionList.innerHTML = data["description-project-maxis"]
+                .map(item => `<li class="mb-2">${item}</li>`)
+                .join('');
+
+            // Для проекта "CodingTimer"
+            document.querySelector('#project_title_timer').textContent = data["project_title_timer"];
+            document.querySelector('#description_project_timer').innerHTML = `<span class="font-semibold">${data["functionality_title_timer"]}</span> ${data["description_project_timer"]}`;
+            const functionalityList = document.querySelector('#functionality_list_timer');
+            functionalityList.innerHTML = data["functionality_list_timer"]
+                .map(item => `<li class="mb-2">${item}</li>`)
+                .join('');
+            document.querySelector('#future_plans_timer').textContent = data["future_plans_timer"];
+            document.querySelector('#future_plans_description_timer').textContent = data["future_plans_description_timer"];
+            document.querySelector('#technologies_title_timer').textContent = data["technologies_title_timer"];
+            document.querySelector('#technologies_list_timer').textContent = data["technologies_list_timer"];
         });
+            // Добавьте сюда обновление других элементов, если необходимо
 }
 
 // Установить язык по умолчанию (например, русский)
 const savedLanguage = localStorage.getItem('language') || 'ru';
 setLanguage(savedLanguage);
+
+const themeToggleBtn = document.getElementById('theme-toggle');
+const htmlElement = document.documentElement;
+const sunIcon = document.getElementById('sun-icon');
+const moonIcon = document.getElementById('moon-icon');
+
+themeToggleBtn.addEventListener('click', () => {
+  if (htmlElement.classList.contains('dark')) {
+    console.log('Switching to light theme');
+    htmlElement.classList.remove('dark');
+    localStorage.setItem('theme', 'light');
+    sunIcon.classList.add('hidden');
+    moonIcon.classList.remove('hidden');
+  } else {
+    console.log('Switching to dark theme');
+    htmlElement.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
+    sunIcon.classList.remove('hidden');
+    moonIcon.classList.add('hidden');
+  }
+});
+
+// Установка темы при загрузке страницы
+const savedTheme = localStorage.getItem('theme') || 'light';
+if (savedTheme === 'dark') {
+  htmlElement.classList.add('dark');
+  sunIcon.classList.remove('hidden');
+  moonIcon.classList.add('hidden');
+} else {
+  htmlElement.classList.remove('dark');
+  sunIcon.classList.add('hidden');
+  moonIcon.classList.remove('hidden');
+}
+
